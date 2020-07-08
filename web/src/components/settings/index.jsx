260327c08ef2a370ee1663d87d8869fed2a674f7
Lugana707 <sam.m.albon@outlook.com>
2020-07-08 17:49:35 -0400

    Moved description of Asana API Key to tooltip

--- web/src/components/settings/index.jsx
+++ web/src/components/settings/index.jsx
@@ -1,11 +1,26 @@
 import React, { useReducer } from "react";
-import { Container, Button, Row, Col, Form } from "react-bootstrap";
+import {
+  Container,
+  Button,
+  Row,
+  Col,
+  Form,
+  Tooltip,
+  OverlayTrigger
+} from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleNotch, faSave } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCircleNotch,
+  faSave,
+  faExternalLinkAlt,
+  faInfoCircle
+} from "@fortawesome/free-solid-svg-icons";
 import { useSelector, useDispatch } from "react-redux";
 import { updateSettings } from "../../scripts/redux/actions/settingsActions";
 
 const Settings = ({ history }) => {
+  const asanaDeveloperConsoleUrl = "https://app.asana.com/0/developer-console";
+
   const { loading, asanaApiKey } = useSelector(state => state.settings);
   const [settings, setSettings] = useReducer(
     (accumulator, currentValue) => ({ ...accumulator, ...currentValue }),
@@ -21,15 +36,42 @@ const Settings = ({ history }) => {
     history.push("/");
   };
 
+  const ApiKeyTooltip = props => (
+    <Tooltip id="api-key-tooltip" {...props}>
+      this will be used to get data from Asana
+    </Tooltip>
+  );
+
   return (
     <Container>
       <Form className="text-left" onSubmit={handleSubmit}>
         <fieldset disabled={loading}>
           <Row className="pb-2">
             <Form.Group as={Col} xs="12" controlId="formAsanaApiKey">
-              <Form.Label>Asana API Key</Form.Label>
+              <Form.Label>
+                <span className="pr-2">Asana API Key</span>
+                <OverlayTrigger
+                  placement="right"
+                  delay={{ show: 250, hide: 400 }}
+                  overlay={ApiKeyTooltip}
+                >
+                  <FontAwesomeIcon icon={faInfoCircle} size="1x" />
+                </OverlayTrigger>
+              </Form.Label>
               <Form.Text className="text-muted pb-3">
-                this will be used to get data from Asana
+                <span className="d-block">
+                  <span>you can generate an API Key from </span>
+                  <a
+                    className=""
+                    href={asanaDeveloperConsoleUrl}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    aria-link={asanaDeveloperConsoleUrl}
+                  >
+                    <span>developer console </span>
+                    <FontAwesomeIcon icon={faExternalLinkAlt} size="1x" />
+                  </a>
+                </span>
               </Form.Text>
               <Form.Control
                 type="text"
