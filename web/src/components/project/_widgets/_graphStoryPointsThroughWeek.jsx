5bdf07cf818d9505d18a55ff604e13e176487ae4
Lugana707 <sam.m.albon@outlook.com>
2020-07-12 16:30:14 -0400

    Changed story points through week widget to sort data based on sprint start day

--- web/src/components/project/_widgets/_graphStoryPointsThroughWeek.jsx
+++ web/src/components/project/_widgets/_graphStoryPointsThroughWeek.jsx
@@ -4,9 +4,12 @@ import { useSelector } from "react-redux";
 import moment from "moment";
 
 const GraphStoryPointsThroughWeek = () => {
+  const { sprintStartDay = 2 } = useSelector(state => state.settings);
   const { loading, asanaProjectTasks = [] } = useSelector(
     state => state.asanaProjectTasks
   );
+
+  const hideWeekends = true;
 
   const projectTasks = (asanaProjectTasks || []).filter(
     ({ archived }) => !!archived
@@ -32,12 +35,42 @@ const GraphStoryPointsThroughWeek = () => {
             },
             [...fullWeek]
           )
-          .sort(
-            ([weekDayA], [weekDayB]) =>
-              moment().day(weekDayA) - moment().day(weekDayB)
+          .filter(
+            ([weekday]) =>
+              !hideWeekends ||
+              !(
+                weekday ===
+                  moment()
+                    .weekday(6)
+                    .format("dddd") ||
+                weekday ===
+                  moment()
+                    .weekday(7)
+                    .format("dddd")
+              )
           )
+          .sort(([weekDayA], [weekDayB]) => {
+            console.debug("Hello weekday!", moment().weekday(6));
+            const momentA =
+              parseInt(
+                moment()
+                  .day(weekDayA)
+                  .add(-sprintStartDay, "Days")
+                  .format("d"),
+                10
+              ) % 7;
+            const momentB =
+              parseInt(
+                moment()
+                  .day(weekDayB)
+                  .add(-sprintStartDay, "Days")
+                  .format("d"),
+                10
+              ) % 7;
+            return momentA - momentB;
+          })
       })),
-    [projectTasks, fullWeek]
+    [projectTasks, fullWeek, sprintStartDay]
   );
 
   const series = useCallback((series, index) => {
