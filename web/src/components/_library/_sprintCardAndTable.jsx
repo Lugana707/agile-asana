628e1be135d155e7719bea65b37ef86b487a8180
Lugana707 <sam.m.albon@outlook.com>
2020-08-19 18:15:23 -0400

    Fixed styling issues with sprintCardAndTable card

--- web/src/components/_library/_sprintCardAndTable.jsx
+++ web/src/components/_library/_sprintCardAndTable.jsx
@@ -1,6 +1,5 @@
-import React, { useMemo } from "react";
-import { Container, Row, Col, Card } from "react-bootstrap";
-import { useSelector } from "react-redux";
+import React from "react";
+import { Col, Card } from "react-bootstrap";
 import moment from "moment";
 import Table from "./_table";
 import SprintTaskTableRow from "./_sprintTaskTableRow";
@@ -10,22 +9,22 @@ const SprintCardAndTable = ({ sprint }) => {
 
   return (
     <>
-      <Col xs={3} className="pr-1">
+      <Col key={number} xs={3} className="pr-1">
         <Card bg="dark" text="light" className="text-left h-100">
           <Card.Body>
-            <Card.Title className="float-left">
-              {moment(finishedOn).format("YYYY-MM-DD")}
+            <Card.Title>
+              <h1 className="float-right text-info">{number}</h1>
+              <span>{moment(finishedOn).format("YYYY-MM-DD")}</span>
             </Card.Title>
-            <Card.Title as="h1" className="float-right text-info">
-              {number}
-            </Card.Title>
-            <Card.Subtitle className="text-muted">
-              <span className="text-nowrap">{storyPoints} story points</span>
-            </Card.Subtitle>
+            {storyPoints && (
+              <Card.Subtitle className="text-muted">
+                <span className="text-nowrap">{storyPoints} story points</span>
+              </Card.Subtitle>
+            )}
           </Card.Body>
         </Card>
       </Col>
-      <Col xs={9} className="pl-1">
+      <Col className="pl-1">
         <Table className="mt-1 mb-1" data={tasks} row={SprintTaskTableRow} />
       </Col>
     </>
