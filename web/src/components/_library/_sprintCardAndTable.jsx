c0921cdf72812d7ccd33c900a964d3165578891d
Lugana707 <sam.m.albon@outlook.com>
2020-08-19 19:12:12 -0400

    Fixed visual issue with sprintCardAndTable for "commitments at risk"

--- web/src/components/_library/_sprintCardAndTable.jsx
+++ web/src/components/_library/_sprintCardAndTable.jsx
@@ -4,13 +4,23 @@ import moment from "moment";
 import Table from "./_table";
 import SprintTaskTableRow from "./_sprintTaskTableRow";
 
+const determineVariants = variant => {
+  if (variant === "danger") {
+    return { card: "danger", table: "dark" };
+  }
+
+  return { card: "dark" };
+};
+
 const SprintCardAndTable = ({ title, sprint, variant }) => {
   const { tasks, number, storyPoints, finishedOn } = sprint;
+
+  const variants = determineVariants(variant);
 
   return (
     <>
       <Col key={number} xs={3} className="pr-1">
-        <Card bg={variant || "dark"} text="light" className="text-left h-100">
+        <Card bg={variants.card} text="light" className="text-left h-100">
           <Card.Body>
             <Card.Title>
               <h1 className="float-right text-info">{number}</h1>
@@ -29,7 +39,7 @@ const SprintCardAndTable = ({ title, sprint, variant }) => {
           className="mt-1 mb-1"
           data={tasks}
           row={SprintTaskTableRow}
-          variant={variant}
+          variant={variants.table}
         />
       </Col>
     </>
