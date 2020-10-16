6ee3184b2e9206f9a351c963221ab10fdb584a33
MacLure <malcolm.maclure@gmail.com>
2020-10-16 18:09:06 -0400

    ping

--- web/src/routes/sprint/report.jsx
+++ web/src/routes/sprint/report.jsx
@@ -1,17 +1,64 @@
 import React, { useMemo } from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import { useSelector } from "react-redux";
-import moment from "moment";
+import { Container, Jumbotron, Row, Col } from "react-bootstrap";
 import Table from "../../components/library/table";
+import collect from "collect.js";
+import { useSelector } from "react-redux";
 
-const Report = () => {
-  return (
-    <Container>
-      <Row>
-        <Col>Hello Report!</Col>
-      </Row>
+const Report = ({match}) => {
+  const { uuid } = match.params;
+
+  const { sprints } = useSelector(state => state.sprints);
+
+  const sprintMemo = useMemo(() => collect(sprints).firstWhere("uuid", uuid), [
+    sprints,
+    uuid
+  ]);
+
+  if (!sprintMemo) {
+    return <div className="loading-spinner centre" />;
+  }
+
+  const {
+    name,
+    number,
+    startOn,
+    dueOn,
+    storyPoints,
+    completedStoryPoints,
+    averageCompletedStoryPoints,
+    state
+  } = sprintMemo;
+  
+  return <>
+      <Jumbotron fluid className="bg-primary text-left">
+        <Container>
+          <h1>Sprint {number}</h1>
+        </Container>
+      </Jumbotron>
+
+      <Container>
+        <Row>
+          <Col>
+            <h1>Comparison</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h2>Sprint Breakdown</h2>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h1>Summary</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h2>__ Commitments Met</h2>
+          </Col>
+        </Row>
     </Container>
-  );
+  </>;
 };
 
 export default Report;
