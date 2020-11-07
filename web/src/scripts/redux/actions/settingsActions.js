c44b252ef92503f238a6698b32df81572a2f3ebf
Lugana707 <sam.m.albon@outlook.com>
2020-11-07 15:50:48 -0500

    Fixed issue with loading the user when there's no valid api key

--- web/src/scripts/redux/actions/settingsActions.js
+++ web/src/scripts/redux/actions/settingsActions.js
@@ -1,16 +1,43 @@
-import asana from "asana";
+import Asana from "asana";
+import Logger from "js-logger";
 
 const SET_LOADING_SETTINGS = "SET_LOADING_SETTINGS";
 const ADD_SETTINGS = "ADD_SETTINGS";
+const DELETE_SETTINGS = "DELETE_SETTINGS";
 
 const loadUser = () => {
   return async (dispatch, getState) => {
     const { asanaApiKey } = getState().settings;
 
-    const client = asana.Client.create().useAccessToken(asanaApiKey);
-    const user = await client.users.me();
+    if (!asanaApiKey) {
+      dispatch(logout);
+      return false;
+    }
 
-    dispatch({ type: ADD_SETTINGS, loading: false, value: { user } });
+    try {
+      const client = Asana.Client.create().useAccessToken(asanaApiKey);
+      const user = await client.users.me();
+
+      dispatch({ type: ADD_SETTINGS, loading: false, value: { user } });
+    } catch (e) {
+      dispatch(logout);
+      Logger.error(e);
+    }
+  };
+};
+
+const logout = () => {
+  return dispatch => {
+    dispatch({
+      type: DELETE_SETTINGS,
+      loading: false,
+      value: "user"
+    });
+    dispatch({
+      type: DELETE_SETTINGS,
+      loading: false,
+      value: "asanaApiKey"
+    });
   };
 };
 
