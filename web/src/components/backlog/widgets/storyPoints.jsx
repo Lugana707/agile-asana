a2a1855baee68eda302c23c3eeec7f66f1578d5b
Lugana707 <sam.m.albon@outlook.com>
2020-11-11 21:21:16 -0500

    Added widget to display current backlog size

---
+++ web/src/components/backlog/widgets/storyPoints.jsx
@@ -0,0 +1,42 @@
+import React, { useMemo } from "react";
+import { LinkContainer } from "react-router-bootstrap";
+import { Card, Row, Col } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import moment from "moment";
+import collect from "collect.js";
+
+const BacklogStoryPoints = () => {
+  const { refinedBacklogTasks } = useSelector(
+    state => state.refinedBacklogTasks
+  );
+  const { unrefinedBacklogTasks } = useSelector(
+    state => state.unrefinedBacklogTasks
+  );
+
+  const storyPoints = useMemo(() => {
+    return collect(unrefinedBacklogTasks || [])
+      .merge(refinedBacklogTasks || [])
+      .filter()
+      .where("storyPoints")
+      .sum("storyPoints");
+  }, [unrefinedBacklogTasks, refinedBacklogTasks]);
+
+  return (
+    <LinkContainer to="/backlog/dashboard" className="btn p-0">
+      <Card bg="info" text="dark" className="text-left">
+        <Card.Body>
+          <Card.Title className="m-0">
+            <Row>
+              <Col xs={4} as="h1" className="text-nowrap">
+                {storyPoints}
+              </Col>
+              <Col>Backlog Story Points</Col>
+            </Row>
+          </Card.Title>
+        </Card.Body>
+      </Card>
+    </LinkContainer>
+  );
+};
+
+export default BacklogStoryPoints;
--- web/src/routes/backlog/dashboard.jsx
+++ web/src/routes/backlog/dashboard.jsx
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faExclamationTriangle } from "@fortawesome/free-solid-svg-icons";
 import TasksAtRiskWidget from "../../components/backlog/alerts/tasksAtRiskWidget";
 import GraphCountOverTime from "../../components/backlog/widgets/graphCountOverTime";
+import BacklogStoryPoints from "../../components/backlog/widgets/storyPoints";
 
 const Forecast = () => {
   return (
@@ -21,12 +22,15 @@ const Forecast = () => {
           <Col xs={12} md={4} lg={2}>
             <TasksAtRiskWidget />
           </Col>
-          <Col xs={12} md={8} lg={10}>
-            <div className="w-100 bg-warning text-dark rounded pt-4 pb-4">
-              <h2 className="mx-auto">
+          <Col xs={12} md={4} lg={2}>
+            <BacklogStoryPoints />
+          </Col>
+          <Col xs={12} md={4} lg={8}>
+            <div className="w-100 h-100 bg-warning text-dark rounded pt-4 pb-4">
+              <h4 className="mx-auto">
                 <FontAwesomeIcon icon={faExclamationTriangle} />
                 <span> Work In Progress!</span>
-              </h2>
+              </h4>
             </div>
           </Col>
         </Row>
