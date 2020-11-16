5d7b9ffb446ace63fcb09dc70e11a9c983c7fb1f
Lugana707 <sam.m.albon@outlook.com>
2020-11-15 20:52:58 -0500

    Changed tag breakdown to percentage based view for multiple sprints

--- web/src/components/sprint/widgets/graphTagBreakdown.jsx
+++ web/src/components/sprint/widgets/graphTagBreakdown.jsx
@@ -5,32 +5,56 @@ import collect from "collect.js";
 const GraphStoryPointsTrend = ({ sprints }) => {
   const sprintsCollection = useMemo(() => collect(sprints), [sprints]);
 
+  const single = useMemo(() => sprintsCollection.count() === 1, [
+    sprintsCollection
+  ]);
+
   const data = useMemo(() => {
     return sprintsCollection
       .sortBy("number")
       .flatMap(sprint => sprint.tasks)
       .flatMap(task => task.tags)
       .unique()
-      .map(key => ({
-        label: key,
-        data: sprintsCollection
-          .map(sprint => [
-            sprint.number,
-            collect(sprint.tasks)
-              .flatMap(task => task.tags)
-              .where(null, key)
-              .count()
-          ])
-          .all()
-      }))
+      .when(single, collection =>
+        collect([
+          collection
+            .map(key => {
+              const count = collect(sprintsCollection.first().tasks)
+                .flatMap(({ tags }) => tags)
+                .where(null, key)
+                .count();
+
+              return {
+                primary: key,
+                secondary: count
+              };
+            })
+            .dump()
+            .toArray()
+        ])
+      )
+      .when(!single, collection =>
+        collection.map(key => ({
+          label: key,
+          data: sprintsCollection
+            .map(({ number, tasks }) => {
+              const taskTags = collect(tasks).flatMap(({ tags }) => tags);
+
+              const count = taskTags.where(null, key).count();
+
+              return [number, (count / parseFloat(taskTags.count())) * 100];
+            })
+            .all()
+        }))
+      )
       .all();
-  }, [sprintsCollection]);
+  }, [sprintsCollection, single]);
 
   const series = useMemo(
     () => ({
-      type: "bar"
+      type: single ? "bar" : "area"
     }),
-    []
+    [single]
   );
   const axes = useMemo(
     () => [
@@ -38,19 +62,24 @@ const GraphStoryPointsTrend = ({ sprints }) => {
         primary: true,
         type: "ordinal",
         position: "bottom",
-        show: sprintsCollection.count() > 1
+        show: true
       },
       {
         position: "left",
         type: "linear",
-        stacked: sprintsCollection.count() > 1,
-        hardMin: 0
+        stacked: !single,
+        hardMin: 0,
+        hardMax: !single && 100,
+        format: d => (single ? d : `${Math.round(d)}%`),
+        show: single
       }
     ],
-    [sprintsCollection]
+    [single]
   );
 
-  return <Chart data={data} series={series} axes={axes} tooltip dark />;
+  return (
+    <Chart data={data} series={series} axes={axes} tooltip={!single} dark />
+  );
 };
 
 export default GraphStoryPointsTrend;
