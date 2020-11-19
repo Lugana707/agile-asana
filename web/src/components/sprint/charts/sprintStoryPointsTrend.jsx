c7abafca470e012698f25d5b2a246bbcd36b0652
Lugana707 <sam.m.albon@outlook.com>
2020-11-19 16:24:02 -0500

    Fixed #49 using ChartJS for sprintStoryPointsTrend

--- web/src/components/sprint/charts/sprintStoryPointsTrend.jsx
+++ web/src/components/sprint/charts/sprintStoryPointsTrend.jsx
@@ -1,12 +1,12 @@
-import React, { useMemo, useCallback } from "react";
-import { Chart } from "react-charts";
+import React, { useMemo } from "react";
+import { Bar } from "react-chartjs-2";
 import collect from "collect.js";
+import randomFlatColors from "random-flat-colors";
 
 const SprintStoryPointsTrend = ({ sprints = [] }) => {
-  const sprintsCollection = useMemo(
-    () => collect(sprints).sortByDesc("number"),
-    [sprints]
-  );
+  const sprintsCollection = useMemo(() => collect(sprints).sortBy("number"), [
+    sprints
+  ]);
 
   const completedSprints = useMemo(
     () => sprintsCollection.where("state", "COMPLETED"),
@@ -14,63 +14,103 @@ const SprintStoryPointsTrend = ({ sprints = [] }) => {
   );
 
   const data = useMemo(
-    () => [
-      {
-        label: "3 Week Average",
-        data: completedSprints
-          .map(obj => [obj.number, obj.averageCompletedStoryPoints])
-          .reverse()
-          .all()
-      },
-      {
-        label: "Overall Trend",
-        data: completedSprints
-          .whereIn("number", [
-            completedSprints.min("number"),
-            completedSprints.max("number")
-          ])
-          .map(obj => [obj.number, obj.averageCompletedStoryPoints])
-          .all()
-      },
-      {
-        label: "Committed Story Points",
-        data: sprintsCollection
-          .map(obj => [obj.number, obj.storyPoints])
-          .reverse()
-          .all()
-      },
-      {
-        label: "Completed Story Points",
-        data: sprintsCollection
-          .map(obj => [obj.number, obj.completedStoryPoints])
-          .reverse()
-          .all()
-      }
-    ],
+    () => ({
+      labels: sprintsCollection.pluck("number").toArray(),
+      datasets: [
+        {
+          label: "3 Week Average",
+          type: "line",
+          color: randomFlatColors("gray"),
+          data: completedSprints.pluck("averageCompletedStoryPoints").toArray()
+        },
+        {
+          label: "Overall Trend",
+          type: "line",
+          color: randomFlatColors("white"),
+          spanGaps: true,
+          data: sprintsCollection
+            .pluck("number")
+            .map(number => {
+              const min = completedSprints.min("number");
+              const max = completedSprints.max("number");
+
+              switch (number) {
+                case min:
+                  return min;
+                case max:
+                  return max;
+                default:
+                  return undefined;
+              }
+            })
+            .map(number => {
+              if (number) {
+                return completedSprints.firstWhere("number", number)
+                  .averageCompletedStoryPoints;
+              }
+
+              return undefined;
+            })
+            .toArray(),
+          dataOld: completedSprints
+            .whereIn("number", [
+              completedSprints.min("number"),
+              completedSprints.max("number")
+            ])
+            .pluck("averageCompletedStoryPoints")
+            .toArray()
+        },
+        {
+          label: "Committed Story Points",
+          type: "bar",
+          color: randomFlatColors("orange"),
+          data: sprintsCollection.pluck("storyPoints").toArray()
+        },
+        {
+          label: "Completed Story Points",
+          type: "bar",
+          color: randomFlatColors("yellow"),
+          data: sprintsCollection.pluck("completedStoryPoints").toArray()
+        }
+      ].map(({ color, ...obj }) => ({
+        ...obj,
+        borderColor: color,
+        backgroundColor: color,
+        fill: false,
+        borderWidth: 1
+      }))
+    }),
     [sprintsCollection, completedSprints]
   );
 
-  const series = useCallback((series, index) => {
-    switch (index) {
-      case 0:
-      case 1:
-        return { type: "line", position: "bottom" };
-      case 2:
-      case 3:
-      default:
-        return { type: "bar", position: "bottom" };
-    }
-  }, []);
-
-  const axes = useMemo(
-    () => [
-      { primary: true, type: "ordinal", position: "bottom" },
-      { position: "left", type: "linear", stacked: false, hardMin: 0 }
-    ],
+  const options = useMemo(
+    () => ({
+      responsive: true,
+      showLines: true,
+      maintainAspectRatio: false,
+      scales: {
+        xAxes: [
+          {
+            gridLines: { display: true },
+            scaleLabel: { display: false }
+          }
+        ],
+        yAxes: [
+          {
+            gridLines: { display: false },
+            display: true,
+            stacked: false,
+            type: "linear",
+            scaleLabel: { display: false },
+            ticks: { min: 0 }
+          }
+        ]
+      }
+    }),
     []
   );
 
-  return <Chart data={data} series={series} axes={axes} tooltip dark />;
+  return <Bar data={data} options={options} />;
 };
 
 export default SprintStoryPointsTrend;
