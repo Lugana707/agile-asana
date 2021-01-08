6a6bc243b59acd0413cb75b43305e0b9b9504c1b
Lugana707 <sam.m.albon@outlook.com>
2021-01-08 18:31:51 -0500

    Changed NoData to have top padding

--- packages/web/src/components/library/noData.jsx
+++ packages/web/src/components/library/noData.jsx
@@ -4,7 +4,7 @@ import { faSkull } from "@fortawesome/free-solid-svg-icons";
 
 export default ({ noDataText = "Nothing to show!" }) => {
   return (
-    <h2 className="text-center">
+    <h2 className="text-center h-100 pt-4">
       <FontAwesomeIcon icon={faSkull} size="1x" />
       <span className="pl-2">{noDataText}</span>
     </h2>
