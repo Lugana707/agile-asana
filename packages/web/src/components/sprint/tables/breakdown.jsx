52d2ccd3a30c9e8b1275dc0dbf9b23c5fcafc912
Lugana707 <sam.m.albon@outlook.com>
2021-01-07 22:00:28 -0500

    Refactored sprint breakdown to use useMemo

--- packages/web/src/components/sprint/tables/breakdown.jsx
+++ packages/web/src/components/sprint/tables/breakdown.jsx
@@ -1,72 +1,87 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Button } from "react-bootstrap";
 import collect from "collect.js";
 import { LinkContainer } from "react-router-bootstrap";
 import Table from "../../library/table";
 
 const Report = ({ loading, sprints, numberOfTags = 3 }) => {
-  const popularTags = collect(sprints)
-    .pluck("tasksCompleted")
-    .flatten(1)
-    .pluck("tags")
-    .flatten(1)
-    .countBy()
-    .transform((item, key) => ({ name: key, count: item }))
-    .reduce(
-      (accumulator, currentValue) => accumulator.push(currentValue),
-      collect([])
-    )
-    .sortByDesc("count")
-    .take(numberOfTags)
-    .pluck("name");
+  const popularTags = useMemo(
+    () =>
+      collect(sprints)
+        .pluck("tasksCompleted")
+        .flatten(1)
+        .pluck("tags")
+        .flatten(1)
+        .countBy()
+        .transform((item, key) => ({ name: key, count: item }))
+        .reduce(
+          (accumulator, currentValue) => accumulator.push(currentValue),
+          collect([])
+        )
+        .sortByDesc("count")
+        .take(numberOfTags)
+        .pluck("name"),
+    [sprints, numberOfTags]
+  );
 
-  const data = collect(sprints).map(sprint => {
-    const {
-      uuid,
-      number,
-      storyPoints,
-      completedStoryPoints,
-      tasksCompleted
-    } = sprint;
+  const data = useMemo(
+    () =>
+      collect(sprints)
+        .map(sprint => {
+          const {
+            uuid,
+            number,
+            storyPoints,
+            completedStoryPoints,
+            tasksCompleted
+          } = sprint;
 
-    const percentageComplete =
-      storyPoints === 0
-        ? 100
-        : Math.round((completedStoryPoints / parseFloat(storyPoints)) * 100);
+          const percentageComplete =
+            storyPoints === 0
+              ? 100
+              : Math.round(
+                  (completedStoryPoints / parseFloat(storyPoints)) * 100
+                );
 
-    const tags = collect(tasksCompleted)
-      .pluck("tags")
-      .flatten(1)
-      .countBy()
-      .toJSON();
+          const tags = collect(tasksCompleted)
+            .pluck("tags")
+            .flatten(1)
+            .countBy()
+            .toJSON();
 
-    return {
-      uuid,
-      number,
-      percentageComplete,
-      tags
-    };
-  });
-
-  data.push({
-    uuid: false,
-    number: "",
-    tags: popularTags
-      .map(tag => ({
-        name: tag,
-        value: data
-          .pluck("tags")
-          .pluck(tag)
-          .filter()
-          .avg()
-          .toFixed(2)
-      }))
-      .reduce(
-        (accumulator, { name, value }) => ({ ...accumulator, [name]: value }),
-        {}
-      ),
-    percentageComplete: data.avg("percentageComplete")
-  });
+          return {
+            uuid,
+            number,
+            percentageComplete,
+            tags
+          };
+        })
+        .pipe(collection =>
+          collection.push({
+            uuid: false,
+            number: "",
+            tags: popularTags
+              .map(tag => ({
+                name: tag,
+                value: collection
+                  .pluck("tags")
+                  .pluck(tag)
+                  .filter()
+                  .avg()
+                  .toFixed(2)
+              }))
+              .reduce(
+                (accumulator, { name, value }) => ({
+                  ...accumulator,
+                  [name]: value
+                }),
+                {}
+              ),
+            percentageComplete: collection.avg("percentageComplete")
+          })
+        ),
+    [sprints, popularTags]
+  );
 
   const TableRow = ({ data }) => {
     const { uuid, number, tags, percentageComplete } = data;
