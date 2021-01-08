1c57774591d4b54fca4e3e28f223312d0eea5eca
Lugana707 <sam.m.albon@outlook.com>
2021-01-08 17:56:46 -0500

    Refactored sprintCardAndTable to have separate card from task table

---
+++ packages/web/src/components/sprint/sprintCard.jsx
@@ -0,0 +1,109 @@
+import React, { useMemo } from "react";
+import { Link } from "react-router-dom";
+import { Row, Col, Card } from "react-bootstrap";
+import collect from "collect.js";
+
+const SprintCard = ({ title, sprint, variant, children, ...props }) => {
+  const { uuid, tasks, number, completedAt, finishedOn, state } = sprint;
+
+  const storyPoints = useMemo(
+    () =>
+      collect(tasks)
+        .pluck("storyPoints")
+        .where()
+        .sum(),
+    [tasks]
+  );
+
+  const determineVariants = variant => {
+    switch (variant) {
+      case "danger":
+        return { card: "danger", table: "dark", subtitle: "warning" };
+      case "warning":
+        return { card: "warning", table: "dark", subtitle: "dark" };
+      default:
+        return { card: "dark", subtitle: "muted" };
+    }
+  };
+  const variants = determineVariants(variant);
+
+  const isCurrentSprint = useMemo(() => state === "ACTIVE", [state]);
+  const isCompletedSprint = useMemo(() => state === "COMPLETED", [state]);
+  const isForecastSprint = useMemo(() => state === "FORECAST", [state]);
+
+  const stateVariant = useMemo(
+    () =>
+      (isCurrentSprint && "success") ||
+      (isCompletedSprint && "warning") ||
+      "info",
+    [isCurrentSprint, isCompletedSprint]
+  );
+
+  const ConditionalSprintLink = ({ children }) => {
+    if (!!uuid) {
+      return <Link to={`/sprint/${uuid}`}>{children}</Link>;
+    }
+
+    return <div className="w-100">{children}</div>;
+  };
+
+  const SprintNumber = props => {
+    const sprintNumber = number.toString();
+
+    if (number.props || sprintNumber.length < 4) {
+      return <h1 {...props}>{number}</h1>;
+    }
+
+    const small = sprintNumber.slice(0, sprintNumber.length - 2);
+    const big = sprintNumber.slice(sprintNumber.length - 2);
+
+    return (
+      <span {...props}>
+        <span>{small}</span>
+        <h1>{big}</h1>
+      </span>
+    );
+  };
+
+  return (
+    <Card
+      bg={variants.card}
+      text="light"
+      className="text-left h-100 w-100 mb-2 border-0 overflow-hidden"
+    >
+      <Row>
+        <Col key={uuid} xs={12} md={4} lg={2} className="pr-1">
+          <Card.Body>
+            <Card.Title>
+              <ConditionalSprintLink>
+                <>
+                  <SprintNumber
+                    className={`text-${stateVariant} float-right`}
+                  />
+                  <span className="text-light">
+                    {title ||
+                      (finishedOn && finishedOn.format("YYYY-MM-DD")) ||
+                      (completedAt && completedAt.format("YYYY-MM-DD"))}
+                  </span>
+                  <small className={`d-block text-${stateVariant} mt-1`}>
+                    {isCurrentSprint && "(In Progress)"}
+                    {isCompletedSprint && "Completed"}
+                    {isForecastSprint && "Forecast"}
+                  </small>
+                  <div className="clearfix" />
+                </>
+              </ConditionalSprintLink>
+            </Card.Title>
+            <Card.Subtitle className={`text-${variants.subtitle} text-nowrap`}>
+              {!!storyPoints && <div>{storyPoints} story points</div>}
+              <div>{sprint.tasks.length} tasks</div>
+            </Card.Subtitle>
+          </Card.Body>
+        </Col>
+        {children && <Col className="pl-1 bg-dark">{children}</Col>}
+      </Row>
+    </Card>
+  );
+};
+
+export default SprintCard;
--- packages/web/src/components/sprint/task/sprintCardAndTable.jsx
+++ packages/web/src/components/sprint/task/sprintCardAndTable.jsx
@@ -1,9 +1,9 @@
 import React, { useMemo } from "react";
-import { Link } from "react-router-dom";
-import { Row, Col, Card } from "react-bootstrap";
+import { Col } from "react-bootstrap";
 import collect from "collect.js";
 import Table from "../../library/table";
 import TaskTableRow from "../../task/tableRow";
+import SprintCard from "../sprintCard";
 
 const SprintCardAndTable = ({
   title,
@@ -13,16 +13,7 @@ const SprintCardAndTable = ({
   tags: displayTags,
   ...props
 }) => {
-  const { uuid, tasks, number, completedAt, finishedOn, state } = sprint;
-
-  const storyPoints = useMemo(
-    () =>
-      collect(tasks)
-        .pluck("storyPoints")
-        .where()
-        .sum(),
-    [tasks]
-  );
+  const { tasks } = sprint;
 
   const determineVariants = variant => {
     switch (variant) {
@@ -50,102 +41,28 @@ const SprintCardAndTable = ({
     [tasks, displayTags]
   );
 
-  const isCurrentSprint = useMemo(() => state === "ACTIVE", [state]);
-  const isCompletedSprint = useMemo(() => state === "COMPLETED", [state]);
-  const isForecastSprint = useMemo(() => state === "FORECAST", [state]);
-
-  const stateVariant = useMemo(
-    () =>
-      (isCurrentSprint && "success") ||
-      (isCompletedSprint && "warning") ||
-      "info",
-    [isCurrentSprint, isCompletedSprint]
-  );
-
-  const ConditionalSprintLink = ({ children }) => {
-    if (!!uuid) {
-      return <Link to={`/sprint/${uuid}`}>{children}</Link>;
-    }
-
-    return <div className="w-100">{children}</div>;
-  };
-
-  const SprintNumber = props => {
-    const sprintNumber = number.toString();
-
-    if (number.props || sprintNumber.length < 4) {
-      return <h1 {...props}>{number}</h1>;
-    }
-
-    const small = sprintNumber.slice(0, sprintNumber.length - 2);
-    const big = sprintNumber.slice(sprintNumber.length - 2);
-
-    return (
-      <span {...props}>
-        <span>{small}</span>
-        <h1>{big}</h1>
-      </span>
-    );
-  };
-
   const TaskTable = () => (
-    <Col className="pl-1 bg-dark">
-      <Table
-        className="mb-0"
-        data={filteredTasks}
-        row={TaskTableRow}
-        variant={variants.table}
-      />
-    </Col>
+    <Table
+      className="mb-0"
+      data={filteredTasks}
+      row={TaskTableRow}
+      variant={variants.table}
+    />
   );
 
   if (showSprintCard) {
     return (
-      <Card
-        bg={variants.card}
-        text="light"
-        className="text-left h-100 w-100 mb-2 border-0 overflow-hidden"
-      >
-        <Row>
-          {showSprintCard && (
-            <Col key={uuid} xs={12} md={4} lg={2} className="pr-1">
-              <Card.Body>
-                <Card.Title>
-                  <ConditionalSprintLink>
-                    <>
-                      <SprintNumber
-                        className={`text-${stateVariant} float-right`}
-                      />
-                      <span className="text-light">
-                        {title ||
-                          (finishedOn && finishedOn.format("YYYY-MM-DD")) ||
-                          (completedAt && completedAt.format("YYYY-MM-DD"))}
-                      </span>
-                      <small className={`d-block text-${stateVariant} mt-1`}>
-                        {isCurrentSprint && "(In Progress)"}
-                        {isCompletedSprint && "Completed"}
-                        {isForecastSprint && "Forecast"}
-                      </small>
-                      <div className="clearfix" />
-                    </>
-                  </ConditionalSprintLink>
-                </Card.Title>
-                <Card.Subtitle
-                  className={`text-${variants.subtitle} text-nowrap`}
-                >
-                  {!!storyPoints && <div>{storyPoints} story points</div>}
-                  <div>{sprint.tasks.length} tasks</div>
-                </Card.Subtitle>
-              </Card.Body>
-            </Col>
-          )}
-          <TaskTable />
-        </Row>
-      </Card>
+      <SprintCard title={title} sprint={sprint} variant={variant}>
+        <TaskTable />
+      </SprintCard>
     );
   }
 
-  return <TaskTable />;
+  return (
+    <Col className="pl-1 bg-dark">
+      <TaskTable />
+    </Col>
+  );
 };
 
 export default SprintCardAndTable;
