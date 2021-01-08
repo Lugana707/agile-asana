9b1b5c4e04cb6431b4562b02eb719c1c59947d91
Lugana707 <sam.m.albon@outlook.com>
2021-01-08 17:02:29 -0500

    Changed sprint URL to support numbe and UUID

--- packages/web/src/components/sprint/withSprintFromURL.jsx
+++ packages/web/src/components/sprint/withSprintFromURL.jsx
@@ -5,10 +5,12 @@ export default WrappedComponent =>
   withSprints(({ match, sprints, ...props }) => {
     const { uuid } = match.params;
 
-    const sprint = useMemo(() => sprints.firstWhere("uuid", uuid), [
-      uuid,
-      sprints
-    ]);
+    const sprint = useMemo(
+      () =>
+        sprints.firstWhere("uuid", uuid) ||
+        sprints.firstWhere("number", parseInt(uuid, 10)),
+      [uuid, sprints]
+    );
 
     return <WrappedComponent {...props} sprint={sprint} />;
   });
