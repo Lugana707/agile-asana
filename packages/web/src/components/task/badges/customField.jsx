38f11b299cfbe694ee01213efbcbad2e6eecfb53
Lugana707 <sam.m.albon@outlook.com>
2021-01-21 20:28:46 -0500

    Fixed issue with custom field badge failing to handle colour "none"

--- packages/web/src/components/task/badges/customField.jsx
+++ packages/web/src/components/task/badges/customField.jsx
@@ -1,18 +1,31 @@
 import React from "react";
 import Color from "color";
 import { Badge } from "react-bootstrap";
+import randomFlatColors from "random-flat-colors";
+
+const getSafeColour = colour => {
+  try {
+    return Color(
+      colour.toLowerCase() === "none"
+        ? randomFlatColors()
+        : (colour || randomFlatColors()).split("-")[0]
+    );
+  } catch {
+    return Color(randomFlatColors());
+  }
+};
 
 export default ({ className, customField }) => {
   const { name, color } = customField;
 
-  const colour = (color || "white").split("-")[0];
+  const colour = getSafeColour(color);
 
-  const textColour = Color(colour).isLight() ? "text-dark" : "text-light";
+  const textColour = colour.isLight() ? "text-dark" : "text-light";
 
   return (
     <Badge
       className={`${className || ""} ${textColour} mr-1`}
-      style={{ backgroundColor: colour, borderColor: colour }}
+      style={{ backgroundColor: colour.hex(), borderColor: colour.hex() }}
       pill
     >
       {name}
