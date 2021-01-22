82f62aaca5d1ad583c57b50d0b6108839a5b0540
Lugana707 <sam.m.albon@outlook.com>
2021-01-21 19:42:19 -0500

    Change user badge to display logo if image missing

--- packages/web/src/components/userBadge.jsx
+++ packages/web/src/components/userBadge.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import { Image } from "react-bootstrap";
+import Logo from "../logo.png";
 
 export default ({ user }) => {
   const { name, photo } = user;
 
   return (
     <div className="d-inline-block" style={{ height: "30px" }}>
-      {photo && (
-        <Image
-          src={photo.image_128x128}
-          className="h-100"
-          fluid
-          roundedCircle
-        />
-      )}
+      <Image
+        src={(photo && photo.image_128x128) || Logo}
+        className="h-100"
+        fluid
+        roundedCircle={!!photo}
+      />
+
       {name && <span className="pl-2">{name}</span>}
     </div>
   );
