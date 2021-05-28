4349d0037d09f76a0e45060cb1ef55b71e733d84
Lugana707 <sam.m.albon@outlook.com>
2021-05-28 13:50:09 -0400

    Removed tags and daily trends chart, replaced with burn-down for sprints

--- packages/web/src/routes/home.jsx
+++ packages/web/src/routes/home.jsx
@@ -2,12 +2,11 @@ import React, { useState, useMemo } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import SprintFilter from "../components/sprint/filter";
 import SprintStoryPointsTrend from "../components/sprint/charts/sprintStoryPointsTrend";
-import SprintStoryPointPerDay from "../components/sprint/charts/storyPointsPerDay";
-import SprintTagsAreaChart from "../components/sprint/charts/sprintTagsAreaChart";
 import TasksAtRiskWidget from "../components/backlog/alerts/tasksAtRiskWidget";
 import SprintProgressWidget from "../components/sprint/widgets/progress";
 import BacklogStoryPoints from "../components/backlog/widgets/storyPoints";
 import withSprintsCombined from "../components/sprint/withSprintsCombined";
+import SprintBurnDown from "../components/sprint/charts/burnDown";
 
 const Home = ({ sprints, sprintsCombined }) => {
   const [filteredSprints, setFilteredSprints] = useState(false);
@@ -32,6 +31,22 @@ const Home = ({ sprints, sprintsCombined }) => {
     [filteredSprints, sprints]
   );
 
+  const Widgets = () => (
+    <Row className="mr-4">
+      <Col xs={12} lg={12} style={{ height: "50vh" }}>
+        <SprintStoryPointsTrend sprints={sprintsCombinedForDisplay} />
+      </Col>
+      {sprints.where("isCurrentSprint").map(sprint => (
+        <Col xs={12} lg={6} key={sprint.uuid}>
+          <h4>{sprint.name}</h4>
+          <div>
+            <SprintBurnDown sprint={sprint} />
+          </div>
+        </Col>
+      ))}
+    </Row>
+  );
+
   return (
     <Container fluid className="pt-4">
       <Row className="pb-4">
@@ -51,27 +66,7 @@ const Home = ({ sprints, sprintsCombined }) => {
       {!sprintsForDisplay.length && !sprintsCombinedForDisplay.length ? (
         <div className="loading-spinner centre" />
       ) : (
-        <Row className="mr-4">
-          <Col xs={12} lg={12} style={{ height: "50vh" }}>
-            <SprintStoryPointsTrend sprints={sprintsCombinedForDisplay} />
-          </Col>
-          <Col
-            xs={12}
-            md={{ span: 6, order: 1 }}
-            lg={{ span: 6, order: 2 }}
-            style={{ height: "50vh" }}
-          >
-            <SprintStoryPointPerDay sprints={sprintsForDisplay} />
-          </Col>
-          <Col
-            xs={12}
-            md={{ span: 6, order: 2 }}
-            lg={{ span: 6, order: 1 }}
-            style={{ height: "50vh" }}
-          >
-            <SprintTagsAreaChart sprints={sprintsForDisplay} />
-          </Col>
-        </Row>
+        <Widgets />
       )}
     </Container>
   );
--- packages/web/src/routes/sprint/index.jsx
+++ packages/web/src/routes/sprint/index.jsx
@@ -1,20 +1,85 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import SprintFilter from "../../components/sprint/filter";
 import SprintInfoCard from "../../components/sprint/infoCard";
-import withSprints from "../../components/sprint/withSprints";
+import SprintTagsAreaChart from "../../components/sprint/charts/sprintTagsAreaChart";
+import SprintStoryPointPerDay from "../../components/sprint/charts/storyPointsPerDay";
+import withSprintsCombined from "../../components/sprint/withSprintsCombined";
 
-const Sprints = ({ sprints }) => {
+const Sprints = ({ sprints, sprintsCombined }) => {
+  const [filteredSprints, setFilteredSprints] = useState(false);
+
+  const sprintsForDisplay = useMemo(
+    () =>
+      filteredSprints && filteredSprints.isNotEmpty()
+        ? sprints
+            .whereIn("number", filteredSprints.pluck("number").toArray())
+            .toArray()
+        : [],
+    [filteredSprints, sprints]
+  );
+
+  const SprintFilters = () => (
+    <Col xs={12} className="pb-4">
+      <SprintFilter
+        defaultCount={20}
+        sprints={sprintsCombined}
+        setSprints={setFilteredSprints}
+      />
+    </Col>
+  );
+
+  const SprintsList = () =>
+    sprints.map(sprint => (
+      <Col key={sprint.uuid} xs={12} md={6} lg={4} className="pb-4">
+        <SprintInfoCard sprint={sprint} />
+      </Col>
+    ));
+
   return (
     <Container className="pt-4">
       <Row>
-        {sprints.map(sprint => (
-          <Col key={sprint.uuid} xs={12} md={6} lg={4} className="pb-4">
-            <SprintInfoCard sprint={sprint} />
-          </Col>
-        ))}
+        <SprintsList />
       </Row>
+      {false && (
+        <Tabs>
+          <TabList>
+            <Row>
+              <Tab>All Sprints</Tab>
+              <Tab>Story Points Per Day</Tab>
+              <Tab>Tags Over Time</Tab>
+            </Row>
+          </TabList>
+          <TabPanel>
+            <Row>
+              <SprintsList />
+            </Row>
+          </TabPanel>
+          <TabPanel>
+            <Row>
+              <Col style={{ height: "50vh" }}>
+                <SprintFilters />
+                {sprintsForDisplay.length && (
+                  <SprintStoryPointPerDay sprints={sprintsForDisplay} />
+                )}
+              </Col>
+            </Row>
+          </TabPanel>
+          <TabPanel>
+            <Row>
+              <Col style={{ height: "50vh" }}>
+                <SprintFilters />
+                {sprintsForDisplay.length && (
+                  <SprintTagsAreaChart sprints={sprintsForDisplay} />
+                )}
+              </Col>
+            </Row>
+          </TabPanel>
+        </Tabs>
+      )}
     </Container>
   );
 };
 
-export default withSprints(Sprints);
+export default withSprintsCombined(Sprints);
